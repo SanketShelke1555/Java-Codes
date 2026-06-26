67ce9dc740b4279ca1a8f1d599c1087db9d3c105
SanketShelke1555 <shelkesanket1555@gmail.com>
2026-06-26 17:56:52 +0530

    Curdex Updated

--- Collection/src/collection/CRUDEx.java
+++ Collection/src/collection/CRUDEx.java
@@ -5,40 +5,83 @@ import java.util.Scanner;
 
 public class CRUDEx {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		LinkedList obj = new LinkedList();
-		
-		obj.add("Apple");
-		obj.add("Mango");
-		obj.add("Grapees");
-		obj.add("Banana");
-		
-		System.out.print("Enter Your Choice: ");
-		Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        sc.nextLine();
-        
-        switch (choice) {
-		case 1:
-			obj.add(sc);
-			break;
-		case 2:
-			obj.remove(sc);
-			break;
-			
-		case 3:
-			obj.replaceAll(null);
-			break;
-		
-		default:
-			System.out.println("Invalid Choice..!!");
-			break;
-		}
-        
-        System.out.println(sc);
+    public static void main(String[] args) {
 
-	}
+        LinkedList<String> obj = new LinkedList<>();
 
+        obj.add("Apple");
+        obj.add("Mango");
+        obj.add("Grapes");
+        obj.add("Banana");
+
+        Scanner sc = new Scanner(System.in);
+
+        int choice;
+
+        do {
+
+            System.out.println("\n===== MENU =====");
+            System.out.println("1. Add");
+            System.out.println("2. Delete");
+            System.out.println("3. Update");
+            System.out.println("4. Display");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter Your Choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+            case 1:
+                System.out.print("Enter Fruit Name: ");
+                String addFruit = sc.nextLine();
+                obj.add(addFruit);
+                System.out.println("Fruit Added Successfully!");
+                break;
+
+            case 2:
+                System.out.print("Enter Fruit To Remove: ");
+                String removeFruit = sc.nextLine();
+
+                if (obj.remove(removeFruit)) {
+                    System.out.println("Fruit Removed Successfully!");
+                } else {
+                    System.out.println("Fruit Not Found!");
+                }
+                break;
+
+            case 3:
+                System.out.print("Enter Old Fruit Name: ");
+                String oldFruit = sc.nextLine();
+
+                System.out.print("Enter New Fruit Name: ");
+                String newFruit = sc.nextLine();
+
+                int index = obj.indexOf(oldFruit);
+
+                if (index != -1) {
+                    obj.set(index, newFruit);
+                    System.out.println("Fruit Updated Successfully!");
+                } else {
+                    System.out.println("Fruit Not Found!");
+                }
+                break;
+
+            case 4:
+                System.out.println("Current List: " + obj);
+                break;
+
+            case 5:
+                System.out.println("Thank You!");
+                break;
+
+            default:
+                System.out.println("Invalid Choice!");
+            }
+
+        } while (choice != 5);
+
+        sc.close();
+    }
 }
